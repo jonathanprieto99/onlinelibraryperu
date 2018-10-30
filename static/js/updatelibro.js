23d3b259732f26af8f980cf41c1caa7ff910cabb
Alejandro <alejandro.tang@utec.edu.pe>
2018-10-30 10:44:24 -0500

    PUT para qe¿ue funcnione

---
+++ static/js/updatelibro.js
@@ -0,0 +1,50 @@
+$(function(){
+    var url = "http://localhost:8080/libros";
+
+
+    $("#grid").dxDataGrid({
+        dataSource: DevExpress.data.AspNet.createStore({
+            key: "ID",
+            loadUrl: url ,
+            insertUrl: url ,
+            updateUrl: url ,
+            deleteUrl: url ,
+            onBeforeSend: function(method, ajaxOptions) {
+                ajaxOptions.xhrFields = { withCredentials: true };
+            }
+        }),
+        editing: {
+            allowUpdating: true,
+            allowDeleting: true,
+            allowAdding: true
+        },
+        remoteOperations: {
+            sorting: true,
+            paging: true
+        },
+        paging: {
+            pageSize: 12
+        },
+        pager: {
+            showPageSizeSelector: true,
+            allowedPageSizes: [8, 12, 20]
+        },
+        columns: [{
+            dataField: "ID",
+            dataType: "number",
+            allowEditing: false
+        }, {
+            dataField: "titulo"
+        }, {
+            dataField: "autor"
+        }, {
+            dataField: "nacionalidad"
+        }, {
+            dataField: "genero"
+        }, {
+            dataField: "descripcion"
+        },{
+            dataField: "nombreimagen"
+        }, ],
+    }).dxDataGrid("instance");
+});
