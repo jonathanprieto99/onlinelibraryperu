bfbdf51819a237a50c1e2ad24360406d9fe1e1e4
Jonathan Prieto <jonathan.prieto@me.com>
2018-11-04 08:54:59 -0500

    Version prueba 4

--- basic_app.py
+++ basic_app.py
@@ -350,4 +350,4 @@ def create_app():
 # Start development web server
 if __name__ == '__main__':
     app = create_app()
-    app.run(host=('0.0.0.0'), port=5000, debug=False)
+    app.run(host=('0.0.0.0'), port=4000, debug=False)
