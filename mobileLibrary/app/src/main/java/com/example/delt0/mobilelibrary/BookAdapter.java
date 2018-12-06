22aca7e6fbb1a45615aaa98916de77b368ea95f6
Alejandro <alejandro.tang@utec.edu.pe>
2018-12-05 19:54:30 -0500

    Comentarios para que se pueda entender el problema

--- mobileLibrary/app/src/main/java/com/example/delt0/mobilelibrary/BookAdapter.java
+++ mobileLibrary/app/src/main/java/com/example/delt0/mobilelibrary/BookAdapter.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,17 +18,20 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import java.util.Map;
 
+
+//PARTE 1
+//INICIAR AQUÍ
+
 public class BookAdapter extends RecyclerView.Adapter<BookAdapter.ViewHolder>
 {
     public JSONArray elements;
     private Context context;
-    public String bookId;
 
-    public BookAdapter(JSONArray elements, Context context, String bookId)
+
+    public BookAdapter(JSONArray elements, Context context)
     {
         this.elements = elements;
         this.context = context;
-        this.bookId = bookId;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder
@@ -53,21 +57,35 @@ public class BookAdapter extends RecyclerView.Adapter<BookAdapter.ViewHolder>
     @Override
     public void onBindViewHolder(@NonNull BookAdapter.ViewHolder holder, int position) {
         try {
+            /*Esto lo hizo Diego. Usando el elements (jsonarray recibido como parámetro desde el BooksActivity)
+            se extraen los valores que se quieren...
+             */
             JSONObject element = elements.getJSONObject(position);
             String details = element.getString("autor")+" - "+element.getString("genero");
             final String title = element.getString("titulo");
             final String id = element.getString("ID");
+
+            //... y se añaden a los holders dentro del xml
             holder.first_line.setText(title);
             holder.second_line.setText(details);
 
+            //Con esto los holders pueden ser clickeados
             holder.container.setOnClickListener(new View.OnClickListener(){
 
-                @Override
+                //Esta parte define lo que pasa cuando se hace click
+                @Override //Con esto se redirige a la DisplayActivity
                 public void onClick(View v) {Intent goToDisplay = new Intent(context,DisplayActivity.class);
+
+                    /*Se añaden además otros dos parámetros: el id (para que sepa a cuál libro buscar)
+                     y el título (para ponerlo como título
+                      */
                     goToDisplay.putExtra("ID",id);
                     goToDisplay.putExtra("titulo", title);
+
+                    //Se inicia la DisplayActivity
                     context.startActivity(goToDisplay);
                 }
+                //VER COMENTARIOS DEL DISPLAYACTIVITY ->
             });
 
 
